7822be469d23d235ebde8b48872272136811fbe8
Javi <javicasados@gmail.com>
2018-07-25 17:03:13 +0200

    Created landing component
    - Added landing page as home page.
    - Added in router file
    - Updated app.module.ts file according to the latest changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
+// Core
 import { NgModule } from '@angular/core';
 
-
+// Components
 import { AppComponent } from './app.component';
+import { LandingComponent } from '../pages/landing/landing.component';
 
+// Modules
+import { RouterModule } from '../../node_modules/@angular/router';
+import { appRoutes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
